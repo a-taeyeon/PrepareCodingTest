d1ed92baeee5a1b5a23a0cbca0d98aa1d5f8b3de
a-taeyeon <taeyeon0107@naver.com>
2021-08-31 18:40:29 +0900

    greedy

--- 이것이 취업을 위한 코딩테스트다/Greedy/Chapter03_greedy_2.py
+++ 이것이 취업을 위한 코딩테스트다/Greedy/Chapter03_greedy_2.py
@@ -19,7 +19,8 @@ for i in range(m):
 
 print(sum)
 
-''' 답안 예시 '''
+''' 단순하게 푸는 답안 예시 '''
+# 이 방법은 M의 크기가 100억 이상처럼 커진다면 시간 초과 판정 받음
 '''
 n, m, k = map(int, input().split())
 data = list(map(int, input.split()))
@@ -44,4 +45,26 @@ while True:
   m -= 1 # 더할 때마다 1씩 빼기
 
 print(result) # 최종 답안 출력
+'''
+
+''' 가장 효율적인 답안 '''
+# 반복되는 수열에 대해서 파악
+'''
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort()
+
+first = data[n - 1]
+second = data[n - 2]
+
+# 가장 큰 수가 더해지는 횟수 계산 (수열)
+count = int(m / (k + 1)) * k
+count += m % (k + 1)
+
+result = 0
+result += (count) * first # 가장 큰 수 더하기
+result += (m - count) * second # 두 번째로 큰 수 더하기
+
+print(result)
 '''
